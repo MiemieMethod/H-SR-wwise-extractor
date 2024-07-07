dd754d18e0293ed6bd49124190e3724f14767f7a
方法放寒假 <40489495+MiemieMethod@users.noreply.github.com>
2024-07-07 23:46:48 +0800

    add brute force name generator

--- main.py
+++ main.py
@@ -17,11 +17,29 @@ def fnv_hash_64(data: str):
     return hash_num
 
 
-def outputWwnames(package):
+def addJsonString(json_data, results=[]):
+    if isinstance(json_data, dict):
+        for key in json_data:
+            if key not in results:
+                results.append(key)
+            if isinstance(json_data[key], dict) or isinstance(json_data[key], list):
+                addJsonString(json_data[key], results)
+            elif isinstance(json_data[key], str):
+                if json_data[key] not in results:
+                    results.append(json_data[key])
+    elif isinstance(json_data, list):
+        for item in json_data:
+            if isinstance(item, dict) or isinstance(item, list):
+                addJsonString(item, results)
+            elif isinstance(item, str):
+                if item not in results:
+                    results.append(item)
+
+def outputWwnames():
     result = ""
 
     result += "\n### LANG NAMES\n"
-    for key in package.LANGUAGE_DEF:
+    for key in ["SFX", "Chinese(PRC)", "English", "Japanese", "Korean"]:
         result += f"{key}\n"
 
     result += "\n### BGM NAMES\n"
@@ -115,6 +133,19 @@ def outputWwnames(package):
     with open("wwiser_utils/wwnames/Honkai - Star Rail (PC).txt", "r", encoding="utf-8") as f:
         result += f"\n### WWISER NAMES\n{f.read()}"
 
+    result += "\n### BRUTE FORCE NAMES\n"
+    results = []
+    for root, dirs, files in os.walk("data"):
+        for file in files:
+            if file.endswith(".json"):
+                with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                    if file.startswith("TextMap"):
+                        continue
+                    print(f"[Main] Name fetch: Reading {os.path.join(root, file)}")
+                    data = json.load(f)
+                    addJsonString(data, results)
+    result += "\n".join(results)
+
     with open("output/unpack/wwnames.txt", "w", encoding="utf-8") as f:
         f.write(result)
 
@@ -173,8 +204,6 @@ def unpackWwiseBanks():
                     os.makedirs(f"output/unpack/{i.lower()}/externals")
                 with open(f'output/unpack/{i.lower()}/externals/{j}.wem', 'wb') as f:
                     elegantWrite(f, file_data)
-
-    outputWwnames(package)
 
 
 def extractBankWem():
@@ -485,20 +514,22 @@ def renameEventWems():
 if __name__ == '__main__':
     print("[Main] Start!")
     print("[Main] Start unpacking Wwise banks...")
-    unpackWwiseBanks()
+    # unpackWwiseBanks()
     print("[Main] Start extracting bank wems...")
-    extractBankWem()
+    # extractBankWem()
     # if you just want to unpack but not rename, comment all lines below
+    print("[Main] Start outputting wwnames...")
+    # outputWwnames()
     print("[Main] Start generating bank data...")
     generateBankData()
     print("[Main] Start loading bank xml...")
     loadBankXml()
     print("[Main] Start renaming external wems...")
-    renameExtrenalWems()
+    # renameExtrenalWems()
     print("[Main] Start renaming event wems...")
     renameEventWems()
     print("[Main] Start deleting completed files...")
     # this program will delete the files in the `output/unpack` folder which are successfully renamed.
     # if you want to keep them, comment the line below.
-    deleteCompletedFiles()
+    # deleteCompletedFiles()
     print("[Main] Done!")
